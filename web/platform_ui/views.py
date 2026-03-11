0c953d1d82bce83ba8a15eaae93ead64a98f89a8
Badre Jdkd <elmoudenbadre30@gmail.com>
2026-03-11 13:06:57 +0100

    ajouter quelque fonction dans les autres agents et debuter interface django

--- web/platform_ui/views.py
+++ web/platform_ui/views.py
@@ -1,3 +1,70 @@
+import sys
+import os
+import pandas as pd
+from sqlalchemy import text
+from langchain_ollama import ChatOllama
+from langchain.messages import SystemMessage, HumanMessage
+ 
+# ajouter la racine du projet au path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from django.shortcuts import render
+from pipeline.orchestrator import Orchestrator
+from django.shortcuts import render
+from llm.llm_agent import LLMAgent
 
-# Create your views here.
+def home(request):
+
+    return render(request, "home.html")
+
+
+def analyze_quality(request):
+
+    result = None
+
+    if request.method == "POST":
+
+        db_type = request.POST.get("db_type")
+        db_name = request.POST.get("db_name")
+
+        orchestrator = Orchestrator(db_type, db_name)
+
+        result = orchestrator.run_quality_analysis()
+
+    return render(request, "analyze.html", {"result": result})
+
+
+def run_pipeline(request):
+
+    result = None
+
+    if request.method == "POST":
+
+        db_type = request.POST.get("db_type")
+        db_name = request.POST.get("db_name")
+
+        orchestrator = Orchestrator(db_type, db_name)
+
+        orchestrator.run_pipeline()
+
+        result = "Pipeline executed successfully"
+
+    return render(request, "run.html", {"result": result})
+
+def llm_tables(request):
+
+    result = None
+
+    if request.method == "POST":
+
+        tables = request.POST.get("tables")
+
+        table_list = tables.split(",")
+
+        llm = LLMAgent()
+
+        result = llm.select_tables_for_cleaning(table_list)
+
+    return render(request, "llm_tables.html", {"result": result})
+
+
+    
